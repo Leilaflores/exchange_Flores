16e9bd337e974976a8df6e08ce35aa04c1c42878
Leila Flores <floresleila2411@gmail.com>
2022-04-27 00:31:37 -0300

    agregando LocalStorage

--- JS/crearcuenta.js
+++ JS/crearcuenta.js
@@ -1,18 +1,52 @@
+
+/* if() */
+
+console.log(localStorage.getItem("usuarios"));
+
 let ojo = document.getElementById("show_password");
 
 console.log(this.querySelector);
 
-ojo.addEventListener("click", (mostrarPassword) => {
-    const icon = document.querySelector("#icono");
-    let cambio = document.getElementById("txtPassword");
+ojo.addEventListener("click", showPassword);
 
-    if (cambio.type == "password") {
-        cambio.type = "text";
-        icon.classList.remove("fa-eye-slash");
-        icon.classList.add("fa-eye");
-    } else {
-        cambio.type = "password";
-        icon.classList.remove("fa-eye");
-        icon.classList.add("fa-eye-slash");
+let confirmar = document.getElementById("confirmar");
+
+confirmar.addEventListener("click", enviar);
+
+function enviar() {
+    let nombre = document.getElementById("name").value;
+    let correo = document.getElementById("email").value;
+    let id = document.getElementById("tipoId").value;
+    let numId = document.getElementById("Id").value;
+    let clave = document.getElementById("txtPassword").value;
+    
+    const usuario = {
+        nombre: nombre,
+        correo: correo,
+        tipoId: id,
+        id: numId,
+        password: clave,
     }
-})
+    
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+    
+    const usuarioLocal = JSON.parse(localStorage.getItem("usuario"));
+    
+    console.log(usuarioLocal);
+    console.log(usuarioLocal.nombre);
+}
+
+function showPassword() {
+  const icon = document.querySelector("#icono");
+  let cambio = document.getElementById("txtPassword");
+
+  if (cambio.type == "password") {
+    cambio.type = "text";
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
+  } else {
+    cambio.type = "password";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
+  }
+}
